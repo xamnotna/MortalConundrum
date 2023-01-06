48e762cc81bcf532abf89f6cfce8b9433df95cc8
xamnotna <antonlarssonauna@gmail.com>
2023-01-06 18:50:48 +0100

    animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,9 +15,22 @@ public class PlayerController : MonoBehaviour
     float horizontalInput;
     float verticalInput;
 
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    Vector2 lookDirection = new Vector2(1, 0);
+    // lastMoveDir is used to store the last direction the player was moving in
+    Vector3 lastMoveDir = Vector3.zero;
+
+
+
+    //private Vector3 lastMoveDir = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         rigidbody2d  = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
     }
@@ -28,7 +41,51 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        
+// move 8 directions
+
+
+  
+         Vector2 move = new Vector2(horizontalInput, verticalInput);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        if (move.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (move.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        animator.SetFloat("xMove", lookDirection.x);
+        animator.SetFloat("yMove", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
+        
+
+    } 
+
+
+
+
+/* void UpdateAnimDir ( )
+    {
+        if (lastMoveDir != Vector3.zero)
+        {
+            animator.SetFloat("xMove", lastMoveDir.x);
+            animator.SetFloat("yMove", lastMoveDir.y);
+
+        }
     }
+
+     */
 
     void FixedUpdate()
     {
